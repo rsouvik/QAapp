740f526c307656b516fd50d6149e2775690e6fd0
Souvik Ray <Souvik@sharethis.com>
2014-08-15 00:45:37 -0700

    data model

---
+++ Snattery/SnQuestion.m
@@ -0,0 +1,26 @@
+//
+//  SnQuestion.m
+//  Snattery
+//
+//  Created by Souvik Ray on 8/15/14.
+//  Copyright (c) 2014 com.snattery. All rights reserved.
+//
+
+#import "SnQuestion.h"
+#import "SnUser.h"
+
+
+@implementation SnQuestion
+
+@dynamic createdAt;
+@dynamic objectid;
+@dynamic questxt;
+@dynamic syncStatus;
+@dynamic timer;
+@dynamic timestamp;
+@dynamic tokens;
+@dynamic updatedAt;
+@dynamic qtou;
+@dynamic qtoa;
+
+@end
---
+++ Snattery/SnUser.m
@@ -0,0 +1,22 @@
+//
+//  SnUser.m
+//  Snattery
+//
+//  Created by Souvik Ray on 8/15/14.
+//  Copyright (c) 2014 com.snattery. All rights reserved.
+//
+
+#import "SnUser.h"
+
+
+@implementation SnUser
+
+@dynamic objectid;
+@dynamic rating;
+@dynamic socialID;
+@dynamic tokens;
+@dynamic uname;
+@dynamic utoa;
+@dynamic utoq;
+
+@end
